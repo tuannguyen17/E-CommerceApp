63afa62c82a01d407c922c6b71c301b5a6e4427e
tuannc <tuannguyen17@augustana.edu>
2022-09-02 01:03:41 -0500

    complete user profile layout

---
+++ app/src/main/java/com/example/ecommerceapp/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecommerceapp.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
